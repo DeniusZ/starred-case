72a2212097c0007b2863c0761d3a26c7c26b1b44
Deniss Zubkovs <deniss.zubkovs@issuu.com>
2024-11-14 12:50:13 +0100

    fix db reset error

--- db/db.js
+++ db/db.js
@@ -1,23 +1,18 @@
 const path = require("node:path");
 const sqlite3 = require("sqlite3").verbose();
 
-// Create a connection to the SQLite database
-function createDBConnection() {
-  const db = new sqlite3.Database(
-    path.resolve(__dirname, "starred.db"),
-    (error) => {
-      if (error) {
-        return console.error("Error opening database:", error.message);
-      }
+const db = new sqlite3.Database(
+  path.resolve(__dirname, "starred.db"),
+  (error) => {
+    if (error) {
+      return console.error("Error opening database:", error.message);
     }
-  );
-  console.log("Connection with SQLite has been established");
-  return db;
-}
+    console.log("Connection with SQLite has been established");
+  }
+);
 
 // Get all favorites for a specific user
 function getFavoritesByUserId(userId, callback) {
-  const db = createDBConnection();
   const query = `SELECT job_id FROM favorites WHERE user_id = ?`;
 
   db.all(query, [userId], (err, rows) => {
@@ -29,12 +24,10 @@ function getFavoritesByUserId(userId, callback) {
     const jobIds = rows.map((row) => row.job_id);
     callback(null, jobIds);
   });
-  db.close();
 }
 
 // Add a job to favorites for a specific user
 function addFavoriteJob(userId, jobId, callback) {
-  const db = createDBConnection();
   const query = `INSERT INTO favorites (user_id, job_id) VALUES (?, ?)`;
 
   db.run(query, [userId, jobId], function (err) {
@@ -45,12 +38,10 @@ function addFavoriteJob(userId, jobId, callback) {
     }
     callback(null, { id: this.lastID });
   });
-  db.close();
 }
 
 // Remove a job from favorites for a specific user
 function removeFavoriteJob(userId, jobId, callback) {
-  const db = createDBConnection();
   const query = `DELETE FROM favorites WHERE user_id = ? AND job_id = ?`;
 
   db.run(query, [userId, jobId], function (err) {
@@ -61,12 +52,10 @@ function removeFavoriteJob(userId, jobId, callback) {
     }
     callback(null, { message: "Favorite removed successfully" });
   });
-
-  db.close();
 }
 
 module.exports = {
-  createDBConnection,
+  db,
   getFavoritesByUserId,
   addFavoriteJob,
   removeFavoriteJob,
